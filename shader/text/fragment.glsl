618ef30ffc96f49b51f49ad3371b2d6211b4df63
Felix Schreiber <83061405+Scriptor25@users.noreply.github.com>
2025-01-02 22:52:12 +0100

    added matrices to shader, changed shader versions

--- shader/text/fragment.glsl
+++ shader/text/fragment.glsl
@@ -1,11 +1,11 @@
-#version 330 core
+#version 450 core
 
 in vec2 v_Texture;
 
 out vec4 Color;
 
 uniform sampler2D TEXTURE;
-uniform vec3 COLOR;
+uniform vec3 COLOR = vec3(1.0);
 
 void main() {
     vec4 sampled = vec4(1.0, 1.0, 1.0, texture(TEXTURE, v_Texture).r);
--- shader/text/vertex.glsl
+++ shader/text/vertex.glsl
@@ -1,13 +1,15 @@
-#version 330 core
+#version 450 core
 
 in vec4 Position;
 in vec2 Texture;
 
 out vec2 v_Texture;
 
-uniform mat4 PROJECTION;
+uniform mat4 MODEL = mat4(1.0);
+uniform mat4 VIEW = mat4(1.0);
+uniform mat4 PROJECTION = mat4(1.0);
 
 void main() {
-    gl_Position = PROJECTION * Position;
+    gl_Position = PROJECTION * VIEW * MODEL * Position;
     v_Texture = Texture;
 }
