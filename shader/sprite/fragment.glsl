a50bc95928ba5484945788cd38751960a19b784a
Felix Schreiber <felix.schreiber@auctores.de>
2025-02-10 16:39:07 +0100

    moved scaling and offset of sprite frame into vertex shader

--- shader/sprite/fragment.glsl
+++ shader/sprite/fragment.glsl
@@ -1,13 +1,11 @@
 #version 450 core
 
 uniform sampler2D TEXTURE;
-uniform vec2 FRAME_SCALE;
-uniform vec2 FRAME_OFFSET;
 
 in vec2 v_Texture;
 
 out vec4 Color;
 
 void main() {
-    Color = texture(TEXTURE, v_Texture * FRAME_SCALE + FRAME_OFFSET);
+    Color = texture(TEXTURE, v_Texture);
 }
--- shader/sprite/vertex.glsl
+++ shader/sprite/vertex.glsl
@@ -4,6 +4,9 @@ uniform mat4 MODEL = mat4(1.0);
 uniform mat4 VIEW = mat4(1.0);
 uniform mat4 PROJECTION = mat4(1.0);
 
+uniform vec2 FRAME_SCALE;
+uniform vec2 FRAME_OFFSET;
+
 in vec4 Position;
 in vec2 Texture;
 
@@ -11,5 +14,5 @@ out vec2 v_Texture;
 
 void main() {
     gl_Position = PROJECTION * VIEW * MODEL * Position;
-    v_Texture = Texture;
+    v_Texture = Texture * FRAME_SCALE + FRAME_OFFSET;
 }
